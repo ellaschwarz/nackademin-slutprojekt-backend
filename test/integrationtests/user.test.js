e1c5c04142d955769ebd939d2f248a9776b73c07
Johan Garzon <johanprograma@hotmail.com>
2020-09-20 10:16:28 +0200

    Create integration test: POST /api/register for creating an user

--- test/integrationtests/user.test.js
+++ test/integrationtests/user.test.js
@@ -6,3 +6,52 @@ const Database = require("../../database/database");
 const userModel = require("../../models/userModel");
 const app = require("../../app");
 chai.use(chaiHttp);
+
+describe("Integration for User", function() {
+    before( async () => {
+        try {
+            await Database.connect()
+        } catch (error) {
+            console.log(error)
+        }
+    })
+
+    beforeEach( async () => {
+        // Clear database
+    })
+
+    after( async () => {
+        try {
+            await Database.disconnect()
+        } catch (error) {
+            console.log(error)
+        }
+    })
+
+    it("POST /api/register should create a user", async () => {
+        const person = {
+            email: "pepito@mail.com",
+            password: "12345",
+            name: "Pepito Perez",
+            role: "customer",
+            adress: {
+                street: "Corazongatan 3",
+                zip: "123 56",
+                city: "SuperCity"
+            }
+        }
+        const userToRegister = await userModel.signup(person);
+
+        const resp = await request(app)
+            .post("/api/register")
+            .set("Content-Type", "application/json")
+            .send(userToRegister)
+        expect(resp).to.be.json;
+        expect(resp).to.have.status(201);
+        expect(resp.body).to.include.all.keys(["_id", "email", "password", "name", "role", "adress", "orderHistory"]);
+        expect(resp.body).to.deep.include({ email: "pepito@mail.com", name: "Pepito Perez", role: "customer" });
+        expect(user.password).to.not.equal("12345");
+
+    })
+
+})
