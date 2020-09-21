81fb9987f1c3bcee0c3ba1ac674f6ca9c742fc83
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-09-21 11:02:29 +0200

    Integration test for orders

--- test/integrationtests/order.test.js
+++ test/integrationtests/order.test.js
@@ -1,8 +1,34 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const { expect, request } = require("chai");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { expect, request } = require('chai');
 
-const Database = require("../../database/database");
-const orderModel = require("../../models/orderModel");
-const app = require("../../app");
+const Database = require('../../database/database');
+const orderModel = require('../../models/orderModel');
+const app = require('../../app');
 chai.use(chaiHttp);
+
+describe('Integration for Order', function () {
+	before(async () => {
+		try {
+			await Database.connect();
+		} catch (error) {
+			console.log(error);
+		}
+	});
+
+	beforeEach(async function () {
+		await orderModel.clear();
+	});
+
+	after(async () => {
+		try {
+			await Database.disconnect();
+		} catch (error) {
+			console.log(error);
+		}
+	});
+
+	it('POST /api/orders should create a order', async function () {
+		console.log('hehe');
+	});
+});
